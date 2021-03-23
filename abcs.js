efe5d58a8cd51109c1b468d93d3669a23f0d1389
chocon123 <77884754+chocon123@users.noreply.github.com>
2021-03-23 16:39:42 +0700

    Add files via upload

---
+++ abcs.js
@@ -0,0 +1,34 @@
+// function average(...hocsinh) {
+//     let diem = 0;
+//     for (let i of hocsinh) {
+//         diem += i;
+//     }
+//     diem = (diem / hocsinh.length).toFixed(2);
+//     if (diem <= 5){
+//         alert(`Diểm trung bình là: ${diem}: kems`);
+//     }else if ( diem >= 7){
+//         alert(`Điểm trung bình là: ${diem}: khas`);
+//     }else (diem >= 8)
+//         alert(`Điểm trung bình là: ${diem}: gioir`);
+
+//     }
+    
+
+// average(7)
+
+
+// equation(5, 8, 9, -6);
+// function equation(a,b,c) {
+//     let i;
+//     let delta = b*b-4*a*c;
+//     if (a == 0){
+//         i = -c/b;
+//         console.log(`phương trình có nhiệm duy nhất: ${i}`);
+//     }else if(delta == 0){
+//         i = -b/2*a;
+//         console.log(`phương trình có nhiệm kép: ${i}`);
+//     }else {
+//         console.log("phương trình vô nhiệm");
+//     }
+
+// }
